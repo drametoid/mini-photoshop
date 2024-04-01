1b1d898f16e5b637d0b6dacd1e9486c64cc3ea39
Shubham Bhatia <39002688+drametoid@users.noreply.github.com>
2024-04-01 14:29:19 -0700

    adds images and main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+import tkinter as tk
+from tkinter import filedialog, messagebox, Label, Button
+
+class TinyPhotoshop:
+
+    def __init__(self, root):
+        pass
+
+    def upload_image(self):
+        pass
+
+    def parse_bmp_file(self):
+        pass
+
+    def exit(self):
+        self.root.quit()
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = TinyPhotoshop(root)
+    root.mainloop()
